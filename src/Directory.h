c549961f7fdaa34a14b060374cebfaf7b3c2756d
WangZixu <vw@Moby.local>
2022-05-10 13:47:19 +0800

    Add Directory.cpp

--- src/Directory.h
+++ src/Directory.h
@@ -1,9 +1,13 @@
 #ifndef DIRECTORY_H
 #define DIRECTORY_H
 
+static const int MAX_SUB_DIR_NUM = 12;
+static const int MAX_FILE_NAME = 32;
+
 class Directory
 {
-    int x;
+    unsigned int d_inode_num[MAX_SUB_DIR_NUM];
+    char d_filename[MAX_SUB_DIR_NUM][MAX_FILE_NAME];
 };
 
 #endif
